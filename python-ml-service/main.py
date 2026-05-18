bdabef556df69acedd270aa0e5452c512814c4a5
albimulfi <albi.mulfi@gmail.com>
2026-05-18 20:29:28 +0700

    feat: add fastapi prediction endpoints

--- python-ml-service/main.py
+++ python-ml-service/main.py
@@ -1,15 +1,52 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import pandas as pd
+import joblib
 
-app = FastAPI()
+# Create FastAPI app
+app = FastAPI(
+    title="Pokemon ML Service",
+    description="Pokemon Prediction API using FastAPI and Machine Learning",
+    version="1.0.0"
+)
 
+model = joblib.load("model.pkl")
+
+# Request schema
+class PokemonRequest(BaseModel):
+    hp: int
+    attack: int
+    defense: int
+    speed: int
+
+# Root endpoint
 @app.get("/")
 async def root():
     return {
-        "message": "Pokemon ML Service Running!"
+        "message": "Pokemon ML Service Running"
     }
 
+# Health endpoint
 @app.get("/health")
 async def health():
     return {
         "status": "ok"
+    }
+
+# Predict endpoint
+@app.post("/predict")
+async def predict(data: PokemonRequest):
+    
+    sample = pd.DataFrame([{
+        "hp": data.hp,
+        "attack": data.attack,
+        "defense": data.defense,
+        "speed": data.speed
+    }])
+
+    # Predict
+    prediction = model.predict(sample)
+
+    return {
+        "prediction": prediction[0]
     }
